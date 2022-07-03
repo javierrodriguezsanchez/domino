a5c8ef3634e4866c67e9015da09fd504cace0998
Daniel <danielabad774@gmail.com>
2022-07-02 21:23:56 -0400

    no se habia guardado el cambio en la escena ni el la apk de consola

--- Game/Escena.cs
+++ Game/Escena.cs
@@ -3,26 +3,14 @@ using DominoTable;
 using DominoPlayer;
 public class Escena
 {
-<<<<<<< HEAD
     public IEnumerable<Piece> Tablero;
     public Player JugadorUltimaJugada;
-=======
-    public Piece ultimaFicha;
-    public Player JugadorUltimaJugada;
-    public int posicionUltimaJugada;
->>>>>>> 06d46c5a1a8b04cce1e30f0cdc1440d10b79a4d0
     public Dictionary<Player, List<Piece>> manos;
     int ganador;
     public Escena(Game Juego)
     {
-<<<<<<< HEAD
         Tablero = Juego.Tablero.VerMesaActual();
         JugadorUltimaJugada = Juego.JugadorActual;
-=======
-        ultimaFicha = Juego.jugadas[Juego.jugadas.Count - 1].ficha;
-        JugadorUltimaJugada = Juego.jugadas[Juego.jugadas.Count -1].jugador;
-        posicionUltimaJugada = Juego.jugadas[Juego.jugadas.Count -1].posicion;
->>>>>>> 06d46c5a1a8b04cce1e30f0cdc1440d10b79a4d0
         manos = Juego.manos;
     }
 }
--- Game/Game.cs
+++ Game/Game.cs
@@ -8,11 +8,7 @@ public class Game : IEnumerable<Escena> {
 	public int Turno = 0;
     
 	bool primerTurno = true;
-<<<<<<< HEAD
 	public GamePieces fichasDelJuego;
-=======
-	 GamePieces fichasDelJuego;
->>>>>>> 06d46c5a1a8b04cce1e30f0cdc1440d10b79a4d0
 	//Turno actual
 	public Dictionary<int, List<Player>> equipos = new();
 
@@ -23,34 +19,22 @@ public class Game : IEnumerable<Escena> {
     //Jugador al cual le corresponde jugar
 	public int cantFichasJugadorActual => manos[JugadorActual].Count;
     //Cantidad de fichas en la mano del jugador actual
-<<<<<<< HEAD
-=======
-   
-	public List<(Piece ficha, int posicion, int cara, Player jugador)> jugadas = new(); 
->>>>>>> 06d46c5a1a8b04cce1e30f0cdc1440d10b79a4d0
     
 	public Player[] Jugadores; //Voa cambiar esto despues. Ese array  ta feo
 	public Dictionary<Player, List<Piece>> manos = new();
 	//Diccionario que le hace corresponder su mano a cada jugador
 	public Rules reglas;
 	//Reglas del juego
-<<<<<<< HEAD
 	public Table Tablero = new(); //Mutar despues
 
 	public Game(Player[] jugadores, Rules reglas) {
 		Jugadores = jugadores;
-=======
-	public Table tablero = new(); //Mutar despues
-
-	public Game(Player[] jugadores, Rules reglas) {
->>>>>>> 06d46c5a1a8b04cce1e30f0cdc1440d10b79a4d0
 		this.reglas = reglas;
 		fichasDelJuego = new GamePieces(reglas.Tope);
 		foreach (var jug in jugadores) {
 			if (!equipos.ContainsKey(jug.Equipo)) {
 				equipos.Add(jug.Equipo, new List<Player>());
 			}
-<<<<<<< HEAD
 			equipos[jug.Equipo].Add(jug);
 			manos.Add(jug, new List<Piece>());	
 		}
@@ -58,26 +42,11 @@ public class Game : IEnumerable<Escena> {
 	}
 
 	public void Jugar() => reglas.Turno.Play(JugadorActual, this);
-=======
-
-			equipos[jug.Equipo].Add(jug);
-			manos.Add(jug, new List<Piece>());
-			for (var i = 0; i < reglas.Cantidad_Inicial_En_La_Mano; i++) {
-				manos[jug].Add(fichasDelJuego.TomarUna());
-			}
-		}
-
-		Jugadores = jugadores;
-	}
-
-	public void Jugar() => jugadas.Add(reglas.Turno.Play(JugadorActual, this));
->>>>>>> 06d46c5a1a8b04cce1e30f0cdc1440d10b79a4d0
 
 	public bool SeAcabo() => reglas.Final.EndOfTheGame(this);
 
 	private void AvanzarTurno() => Turno++;
 
-<<<<<<< HEAD
 
 	public IEnumerator<Escena> GetEnumerator() {
 		while (!SeAcabo()) {
@@ -91,24 +60,6 @@ public class Game : IEnumerable<Escena> {
 			yield return new(this);
 		}
 	}
-=======
-
-	public IEnumerator<Escena> GetEnumerator() {
-		while (!SeAcabo()) {
-			if(primerTurno){
-            primerTurno = false;
-			}
-			else{
-			AvanzarTurno();
-			}
-			Jugar();
-			yield return new(this);
-		}
-
-		yield return new(this);
-	}
-
->>>>>>> 06d46c5a1a8b04cce1e30f0cdc1440d10b79a4d0
 	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
 }
